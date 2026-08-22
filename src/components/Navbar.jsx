7a9a9d0dada52c72e46690d6580cda7f6e7a69ec
Shakibul Alam <shakibulalam.9020@gmail.com>
2026-08-22 19:10:37 +0600

    Final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
       <header className="bg-teal">
       <div className="max-w-6xl mx-auto px-6 py-5 flex items-center justify-between">
-        <Link to="/" className="px-5 py-2 rounded-full text-sm font-medium bg-teal text-white    hover:bg-teal-dark transition">
+        <Link to="/" className="px-5 py-2 rounded-full text-sm font-medium bg-teal text-white hover:bg-teal-dark transition">
           LevelCare
         </Link>
 
